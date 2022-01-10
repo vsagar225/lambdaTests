694237f9b64e4938a5605829b620648e76bbe331
pvidy <pvidy@LAPTOP-4I1PL9LR>
2022-01-10 10:21:54 +0530

    Sikuli working copy

--- Proj3/src/test/java/pack2/AutoItTest.java
+++ Proj3/src/test/java/pack2/AutoItTest.java
@@ -16,13 +16,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import autoitx4java.AutoItX;
 
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AutoItTest {
 	static WebDriver driver;
 	
 	@Test
-	public void testUpload() throws InterruptedException, IOException, AWTException
+	public void testUpload() throws InterruptedException, IOException, AWTException, FindFailed
 	{
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
@@ -35,18 +39,27 @@ public class AutoItTest {
 
 //		Runtime.getRuntime().exec("C:\\Sagar\\Selenium\\pdf.exe");
 		
+		 String filepath = "C:\\Sagar\\Selenium\\Files\\";
+	        String inputFilePath = "C:\\Sagar\\Selenium\\Files\\";
+	        Screen s = new Screen();
+	        Pattern fileInputTextBox = new Pattern(filepath + "FileTextBox.PNG");
+	        Pattern openButton = new Pattern(filepath + "OpenButton.PNG");
 		
-		AutoItX x = new AutoItX();
-		x.winActivate("Open");
-		boolean active = x.winWaitActive("Open");
-		System.out.println(active);
-		boolean cclick = x.controlClick("Open", "", "Edit1");
-		System.out.println(cclick);
 		
-		boolean text=x.ControlSetText("Open", "", "Edit1", "1");
-		System.out.println(text);
-		Thread.sleep(3000); //This was added just so you could see that the values did change.
+//		AutoItX x = new AutoItX();
+//		x.winActivate("Open");
+//		boolean active = x.winWaitActive("Open");
+//		System.out.println(active);
+//		boolean cclick = x.controlClick("Open", "", "Edit1");
+//		System.out.println(cclick);
+//		
+//		boolean text=x.ControlSetText("Open", "", "Edit1", "1");
+//		System.out.println(text);
+//		Thread.sleep(3000); //This was added just so you could see that the values did change.
 		
+	        s.wait(fileInputTextBox, 20);
+	        s.type(fileInputTextBox, inputFilePath + "Laptop.pdf");
+	        s.click(openButton);
         
 	}	
 	
